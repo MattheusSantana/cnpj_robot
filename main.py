5a49441954ef017680c1ee0fd40228b13a5c52a3
MattheusSantana <matheusess@hotmail.com>
2023-04-09 12:09:57 -0400

    json response

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Response
-from playwright.async_api import async_playwright
 from search_cnpj_robot import search_cnpj
+from fastapi.responses import JSONResponse
+
 
 app = FastAPI()
 
@@ -14,9 +15,11 @@ async def get_cnpj(cnpj):
     if(len(cnpj) != 14 or not cnpj.isdigit()):
         return 'cnpj inválido'
     
-    request = await search_cnpj(cnpj)
-    if (request):
-        with open('page.html') as fh:
+    filename = await search_cnpj(cnpj)
+    if (filename):
+        with open(filename) as fh:
             data = fh.read()
             return Response(content=data, media_type="text/html")
     
+    else:
+        return JSONResponse(content={"204": "NÃO FORAM ENCONTRADAS INFORMAÇÕES PARA O CNPJ INFORMADO"}, status_code=401)
--- search_cnpj_robot.py
+++ search_cnpj_robot.py
@@ -1,49 +1,75 @@
 import time
-import os 
+import os
 from dotenv import load_dotenv
 from playwright.async_api import async_playwright
 from anticaptchaofficial.hcaptchaproxyless import *
 import aiofiles as aiof
+import uuid
+
+
+async def submit_page(page, result):
+    await page.evaluate(f"""
+            let captcha = document.getElementsByName('h-captcha-response')[0];
+            console.log("captcha", captcha);
+            captcha.innerHTML = '{result}' 
+            console.log("inner", captcha);
+            captcha.style.display = 'block';
+            document.getElementById('frmConsulta').submit();""")
+    return print("subimitted()")
+
+# async def submit(page, result):
+#     print("submit")
+#     async with page.expect_response("https://solucoes.receita.fazenda.gov.br/Servicos/cnpjreva/Cnpjreva_Comprovante.asp") as response_info:
+#         print("waiting for response")
+#         response = await response_info.value
+#         print('response', response)
+#         return response.ok
+
 
 async def search_cnpj(cnpj):
     async with async_playwright() as p:
+        filename: False
         browser = await p.chromium.launch(headless=False)
         page = await browser.new_page()
         await page.goto('https://solucoes.receita.fazenda.gov.br/Servicos/cnpjreva/cnpjreva_solicitacao.asp')
         site_key = await page.locator('//*[@id="frmConsulta"]/div[1]/div[2]/div').get_attribute('data-sitekey')
         await page.locator('//*[@id="cnpj"]').fill(cnpj)
+
+        result = await solve_captcha(site_key)
+
+        if (result == False):
+            await browser.close()
+            return False
         
-        result = await solve_captcha(site_key) 
-        print('result', result)
-        await page.evaluate(f"""
-            let captcha = document.getElementsByName('h-captcha-response')[0];
-            console.log('{result}')
-            captcha.innerHTML = '{result}' 
-            captcha.style.display = 'block';
-            document.getElementById('frmConsulta').submit();
-            """)
 
-        content = await page.locator('//*[@id="principal"]').inner_html()    
-        print('content',content)
-        await good(content)
+        await submit_page(page, result)
 
+        # response = await submit(page, result)
+
+
+        content = await page.locator('//*[@id="principal"]').inner_html()
+        filename = await make_html_response(content)
 
         time.sleep(5)
-        
-        await browser.close()
-        return True
 
-async def good(content):
-    async with aiof.open('page.html', "w") as out:
+        await browser.close()
+        return filename
+
+
+async def make_html_response(content):
+    filename = uuid.uuid4().hex
+    filename = filename+'.html'
+    print(filename)
+    async with aiof.open(filename, "w") as out:
         await out.write(content)
         await out.flush()
-        print("done")        
-        return content
+        return filename
 
 
 def handle_response(response):
     print("response body", response.body)
     return response.body
+
 
 async def solve_captcha(site_key):
     load_dotenv()
@@ -51,12 +77,14 @@ async def solve_captcha(site_key):
     solver.set_verbose(1)
     key = os.getenv('APIKEY')
     solver.set_key(key)
-    solver.set_website_url('https://solucoes.receita.fazenda.gov.br/Servicos/cnpjreva/cnpjreva_solicitacao.asp')
+    solver.set_website_url(
+        'https://solucoes.receita.fazenda.gov.br/Servicos/cnpjreva/cnpjreva_solicitacao.asp')
     solver.set_website_key(site_key)
-    result =  solver.solve_and_return_solution()
+    result = solver.solve_and_return_solution()
 
-    if result !=0 :
+    if result != 0:
+        print('result', result)
         return result
     else:
         print(solver.err_string)
-
+        return False
