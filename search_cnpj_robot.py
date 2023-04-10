8229b2674799612f49908584b3f9f4c1e6ca2617
MattheusSantana <matheusess@hotmail.com>
2023-04-09 23:30:58 -0400

    refactoring

--- search_cnpj_robot.py
+++ search_cnpj_robot.py
@@ -7,30 +7,20 @@ import aiofiles as aiof
 import uuid
 
 
-async def submit_page(page, result):
-    await page.evaluate(f"""
-            let captcha = document.getElementsByName('h-captcha-response')[0];
-            console.log("captcha", captcha);
-            captcha.innerHTML = '{result}' 
-            console.log("inner", captcha);
-            captcha.style.display = 'block';
-            document.getElementById('frmConsulta').submit();""")
-    return print("subimitted()")
-
-
+load_dotenv()
 
 async def search_cnpj(cnpj):
     async with async_playwright() as p:
-        filename: False
+        filename = False
+        
         browser = await p.chromium.launch(headless=False)
         page = await browser.new_page()
-        await page.goto('https://solucoes.receita.fazenda.gov.br/Servicos/cnpjreva/cnpjreva_solicitacao.asp')
+        await page.goto(os.getenv('WEBSITE_URL'))
         site_key = await page.locator('//*[@id="frmConsulta"]/div[1]/div[2]/div').get_attribute('data-sitekey')
         await page.locator('//*[@id="cnpj"]').fill(cnpj)
-
+        
         result = await solve_captcha(site_key)
-
-        if (result == False):
+        if (not result):
             await browser.close()
             return False
 
@@ -44,6 +34,30 @@ async def search_cnpj(cnpj):
         await browser.close()
         return filename
 
+async def solve_captcha(site_key):
+    solver = hCaptchaProxyless()
+    solver.set_verbose(1)
+    key = os.getenv('APIKEY')
+    solver.set_key(key)
+    solver.set_website_url(os.getenv('WEBSITE_URL'))
+    solver.set_website_key(site_key)
+    result = solver.solve_and_return_solution()
+
+    if result != 0:
+        return result
+    else:
+        print(solver.err_string)
+        return False
+
+
+async def submit_page(page, result):
+    await page.evaluate(f"""
+            let captcha = document.getElementsByName('h-captcha-response')[0];
+            console.log("captcha", captcha);
+            captcha.innerHTML = '{result}' 
+            console.log("inner", captcha);
+            captcha.style.display = 'block';
+            document.getElementById('frmConsulta').submit();""")
 
 async def make_html_response(content):
     filename = uuid.uuid4().hex
@@ -53,27 +67,3 @@ async def make_html_response(content):
         await out.write(content)
         await out.flush()
         return filename
-
-
-def handle_response(response):
-    print("response body", response.body)
-    return response.body
-
-
-async def solve_captcha(site_key):
-    load_dotenv()
-    solver = hCaptchaProxyless()
-    solver.set_verbose(1)
-    key = os.getenv('APIKEY')
-    solver.set_key(key)
-    solver.set_website_url(
-        'https://solucoes.receita.fazenda.gov.br/Servicos/cnpjreva/cnpjreva_solicitacao.asp')
-    solver.set_website_key(site_key)
-    result = solver.solve_and_return_solution()
-
-    if result != 0:
-        print('result', result)
-        return result
-    else:
-        print(solver.err_string)
-        return False
